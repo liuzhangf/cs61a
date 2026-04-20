b809585f2540428269741455cc0a9b594f5671ff
liuzhangf <86759403+liuzhangf@users.noreply.github.com>
2026-04-20 17:31:18 +0800

    first commit

---
+++ hw03/hw03.py
@@ -0,0 +1,264 @@
+SOURCE_FILE = __file__
+
+
+def num_eights(num):
+    """Returns the number of times 8 appears as a digit of num.
+
+    >>> num_eights(3)
+    0
+    >>> num_eights(8)
+    1
+    >>> num_eights(88888888)
+    8
+    >>> num_eights(2638)
+    1
+    >>> num_eights(86380)
+    2
+    >>> num_eights(12345)
+    0
+    >>> num_eights(8782089)
+    3
+    >>> from construct_check import check
+    >>> # ban all assignment statements
+    >>> check(SOURCE_FILE, 'num_eights',
+    ...       ['Assign', 'AnnAssign', 'AugAssign', 'NamedExpr', 'For', 'While'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    if num == 0:
+        return 0
+    else :
+        if num % 10 == 8:
+            return 1 + num_eights(num // 10)
+        else:
+            return num_eights(num // 10)
+
+
+def digit_distance(num):
+    """Determines the digit distance of num.
+
+    >>> digit_distance(3)
+    0
+    >>> digit_distance(777) # 0 + 0
+    0
+    >>> digit_distance(314) # 2 + 3
+    5
+    >>> digit_distance(31415926535) # 2 + 3 + 3 + 4 + ... + 2
+    32
+    >>> digit_distance(3464660003)  # 1 + 2 + 2 + 2 + ... + 3
+    16
+    >>> from construct_check import check
+    >>> # ban all loops
+    >>> check(SOURCE_FILE, 'digit_distance',
+    ...       ['For', 'While'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    if num < 100 and num >= 10:
+        return abs(num % 10  - num // 10)
+    elif num < 10:
+        return 0
+    else:
+        last_digit = num % 10
+        second_last_digit = (num // 10) % 10
+        return abs(last_digit - second_last_digit) + digit_distance(num // 10)
+
+def interleaved_sum(num, f_odd, f_even):
+    """Compute the sum f_odd(1) + f_even(2) + f_odd(3) + ..., up
+    to num.
+
+    >>> identity = lambda x: x
+    >>> square = lambda x: x * x
+    >>> triple = lambda x: x * 3
+    >>> interleaved_sum(5, identity, square) # 1   + 2*2 + 3   + 4*4 + 5
+    29
+    >>> interleaved_sum(5, square, identity) # 1*1 + 2   + 3*3 + 4   + 5*5
+    41
+    >>> interleaved_sum(4, triple, square)   # 1*3 + 2*2 + 3*3 + 4*4
+    32
+    >>> interleaved_sum(4, square, triple)   # 1*1 + 2*3 + 3*3 + 4*3
+    28
+    >>> from construct_check import check
+    >>> check(SOURCE_FILE, 'interleaved_sum', ['While', 'For', 'Mod']) # ban loops and %
+    True
+    >>> check(SOURCE_FILE, 'interleaved_sum', ['BitAnd', 'BitOr', 'BitXor']) # ban bitwise operators, don't worry about these if you don't know what they are
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    def helper(cur_num, is_odd):
+        if cur_num <= num:
+            if is_odd:
+                return f_odd(cur_num) + helper(cur_num + 1, not is_odd)
+            else :
+                return f_even(cur_num) + helper(cur_num + 1,  not is_odd)
+        else:
+            return 0
+    return helper(1, True)
+        
+
+def next_smaller_dollar(bill):
+    """Returns the next smaller bill in order."""
+    if bill == 100:
+        return 50
+    if bill == 50:
+        return 20
+    if bill == 20:
+        return 10
+    elif bill == 10:
+        return 5
+    elif bill == 5:
+        return 1
+    elif bill == 1:
+        return None
+
+def count_dollars(sum_needed):
+    """Return the number of ways to make change.
+
+    >>> count_dollars(15)  # 15 $1 bills, 10 $1 & 1 $5 bills, ... 1 $5 & 1 $10 bills
+    6
+    >>> count_dollars(10)  # 10 $1 bills, 5 $1 & 1 $5 bills, 2 $5 bills, 10 $1 bills
+    4
+    >>> count_dollars(20)  # 20 $1 bills, 15 $1 & $5 bills, ... 1 $20 bill
+    10
+    >>> count_dollars(45)  # How many ways to make change for 45 dollars?
+    44
+    >>> count_dollars(100) # How many ways to make change for 100 dollars?
+    344
+    >>> count_dollars(200) # How many ways to make change for 200 dollars?
+    3274
+    >>> from construct_check import check
+    >>> # ban iteration
+    >>> check(SOURCE_FILE, 'count_dollars', ['While', 'For'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    def helper(amount, bill):
+        if amount == 0:
+            return 1
+        elif amount < 0 :
+            return 0
+        elif bill is None:
+            return 0
+        use = helper(amount - bill, bill)
+        not_use = helper(amount, next_smaller_dollar(bill))
+        return use + not_use
+    return helper(sum_needed, 100)
+
+def next_larger_dollar(bill):
+    """Returns the next larger bill in order."""
+    if bill == 1:
+        return 5
+    elif bill == 5:
+        return 10
+    elif bill == 10:
+        return 20
+    elif bill == 20:
+        return 50
+    elif bill == 50:
+        return 100
+    elif bill == 100:
+        return None
+
+def count_dollars_upward(sum_needed):
+    """Return the number of ways to make change using bills.
+
+    >>> count_dollars_upward(15)  # 15 $1 bills, 10 $1 & 1 $5 bills, ... 1 $5 & 1 $10 bills
+    6
+    >>> count_dollars_upward(10)  # 10 $1 bills, 5 $1 & 1 $5 bills, 2 $5 bills, 10 $1 bills
+    4
+    >>> count_dollars_upward(20)  # 20 $1 bills, 15 $1 & $5 bills, ... 1 $20 bill
+    10
+    >>> count_dollars_upward(45)  # How many ways to make change for 45 dollars?
+    44
+    >>> count_dollars_upward(100) # How many ways to make change for 100 dollars?
+    344
+    >>> count_dollars_upward(200) # How many ways to make change for 200 dollars?
+    3274
+    >>> from construct_check import check
+    >>> # ban iteration
+    >>> check(SOURCE_FILE, 'count_dollars_upward', ['While', 'For'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    def helper(amount, bill):
+        if amount == 0:
+            return 1
+        elif amount < 0 :
+            return 0
+        elif bill is None:
+            return 0
+        use = helper(amount - bill, bill)
+        not_use = helper(amount, next_larger_dollar(bill))
+        return use + not_use
+    return helper(sum_needed, 1)
+
+
+def print_move(origin, destination):
+    """Print instructions to move a disk."""
+    print("Move the top disk from rod", origin, "to rod", destination)
+
+def move_stack(num, start, end):
+    """Print the moves required to move num disks on the start pole to the end
+    pole without violating the rules of Towers of Hanoi.
+
+    num -- number of disks
+    start -- a pole position, either 1, 2, or 3
+    end -- a pole position, either 1, 2, or 3
+
+    There are exactly three poles, and start and end must be different. Assume
+    that the start pole has at least num disks of increasing size, and the end
+    pole is either empty or has a top disk larger than the top num start disks.
+
+    >>> move_stack(1, 1, 3)
+    Move the top disk from rod 1 to rod 3
+    >>> move_stack(2, 1, 3)
+    Move the top disk from rod 1 to rod 2
+    Move the top disk from rod 1 to rod 3
+    Move the top disk from rod 2 to rod 3
+    >>> move_stack(3, 1, 3)
+    Move the top disk from rod 1 to rod 3
+    Move the top disk from rod 1 to rod 2
+    Move the top disk from rod 3 to rod 2
+    Move the top disk from rod 1 to rod 3
+    Move the top disk from rod 2 to rod 1
+    Move the top disk from rod 2 to rod 3
+    Move the top disk from rod 1 to rod 3
+    """
+    assert 1 <= start <= 3 and 1 <= end <= 3 and start != end, "Bad start/end"
+    "*** YOUR CODE HERE ***"
+    middle =  -1
+    for i in range(1, 4):
+        if i != start and i != end:
+            middle = i
+
+    if num == 1:
+        print_move(start, end)
+        return 
+    elif num == 2:
+        print_move(start, middle)
+        print_move(start, end)       
+        print_move(middle, end)
+        return 
+
+    print_move(start, end)
+    print_move(start, middle)
+    print_move(end, middle)
+    print_move(start, end)
+    move_stack(num - 1, middle , end)
+
+
+from operator import sub, mul
+
+def make_anonymous_factorial():
+    """Return the value of an expression that computes factorial.
+
+    >>> make_anonymous_factorial()(5)
+    120
+    >>> from construct_check import check
+    >>> # ban any assignments or recursion
+    >>> check(SOURCE_FILE, 'make_anonymous_factorial',
+    ...     ['Assign', 'AnnAssign', 'AugAssign', 'NamedExpr', 'FunctionDef', 'Recursion'])
+    True
+    """
+    return ( lambda f : lambda n : f(f, n) ) (lambda f, k : 1 if k == 1 else k * f(f, k - 1))
+
