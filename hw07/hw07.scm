8e5182ca6fcec9e76249151563bd29d7c3040413
liuzhangf <86759403+liuzhangf@users.noreply.github.com>
2026-04-21 10:50:15 +0800

    second commit

---
+++ hw07/hw07.scm
@@ -0,0 +1,24 @@
+(define (square n) (* n n))
+
+(define (pow base exp) 
+  (if (= exp 0)
+    1
+    (if (= (remainder exp 2) 1)
+      (* base ( pow (* base base) (/ ( - exp 1) 2)))
+      (pow (* base base) (/ exp 2))
+    )
+  )
+)
+
+(define (repeatedly-cube n x)
+  (if (zero? n)
+      x
+      (begin (define y (*  x x x)) ( repeatedly-cube (- n 1) y) )
+    )
+)
+
+(define (cddr s) (cdr (cdr s)))
+
+(define (cadr s) (car (cdr s)))
+
+(define (caddr s) (car (cdr (cdr s))))
